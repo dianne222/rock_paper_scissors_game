39359709f15313b19203b79580efe582b2148b5d
Cris Dianne Casino <crisdianne32@gmail.com>
2026-06-21 17:29:59 +0800

    uses inheritance for humanplayer class

---
+++ players/human_player.py
@@ -0,0 +1,25 @@
+from player import Player
+
+class HumanPlayer(Player):
+
+    def __init__(self, detector):
+        self._detector = detector
+
+    def get_move(self, hands=None):
+
+        if not hands:
+            return None
+
+        hand = hands[0]
+        fingers = self._detector.fingersUp(hand)
+
+        if fingers == [0, 0, 0, 0, 0]:
+            return "rock"
+
+        elif fingers == [1, 1, 1, 1, 1]:
+            return "paper"
+
+        elif fingers == [0, 1, 1, 0, 0]:
+            return "scissors"
+
+        return None
